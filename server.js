7d142d81b624cb36aab8d9387d15f962722ccdd8
mynameiskatchy <karchietrinh@gmail.com>
2020-12-19 04:14:46 -0500

    Test request with token

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
 
 const posts = [
     {
@@ -14,6 +19,15 @@ const posts = [
 
 app.get('/posts', (req, res) => {
     res.json(posts)
+})
+
+app.post('/login', (req, res) => {
+    // Authenticate User
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
 })
 
 app.listen(3000)

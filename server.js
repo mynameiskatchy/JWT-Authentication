ec6b13f64bb674951c341a4edc9996098360a648
mynameiskatchy <karchietrinh@gmail.com>
2021-02-04 11:19:40 -0500

    Separate auth related stuff into serparate server. (remove login section)

--- server.js
+++ server.js
@@ -21,14 +21,7 @@ app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login', (req, res) => {
-    // Authenticate User
-    const username = req.body.username
-    const user = { name: username }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
-})
 
 function authenticateToken(req, res, next) {
     // Check if we have a valid token

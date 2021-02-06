1473b1c7da3e44a70e4b46c53762c10ad8847ffe
mynameiskatchy <karchietrinh@gmail.com>
2021-02-06 10:02:39 -0500

    CHECKPOINT: Check refresh token lets us get, and expires after 15s

--- authServer.js
+++ authServer.js
@@ -6,6 +6,23 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
+// not a good idea in production, 
+// cuz when server restarts, it empties
+let refreshTokens = [] 
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    // check if we already have a refresh token that exists for this
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    // verify refresh token
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
+
 app.post('/login', (req, res) => {
     // Authenticate User
     const username = req.body.username
@@ -13,6 +30,8 @@ app.post('/login', (req, res) => {
 
     const accessToken = generateAccessToken(user)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    
     res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 

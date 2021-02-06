9a04fe19e0e59ec4c5f8d4125989824e4dbb42ca
mynameiskatchy <karchietrinh@gmail.com>
2021-02-06 07:12:13 -0500

    CHECKPOINT: Check that routes expire after 15 seconds

--- authServer.js
+++ authServer.js
@@ -6,28 +6,14 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-const posts = [
-    {
-        username: 'Kyle',
-        title: 'Post 1'
-    },
-    {
-        username: 'Jim',
-        title: 'Post 2'
-    }
-]
-
-app.get('/posts', authenticateToken, (req, res) => {
-    res.json(posts.filter(post => post.username === req.user.name))
-})
-
 app.post('/login', (req, res) => {
     // Authenticate User
     const username = req.body.username
     const user = { name: username }
 
     const accessToken = generateAccessToken(user)
-    res.json({ accessToken: accessToken })
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
 function generateAccessToken(user) {
